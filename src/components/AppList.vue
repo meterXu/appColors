9418abf3948c2b2267f0109b466736c7846660e8
isaac <xuhonggang@hotmail.com>
2022-05-18 15:39:52 +0800

    ....

---
+++ src/components/AppList.vue
@@ -0,0 +1,49 @@
+<template>
+  <ul class="app-list">
+    <li v-for="(item,index) in 10" :key="index">
+      <el-card shadow="hover" :body-style="{ padding: '0px' }">
+        <img src="/imgs/demo.png" class="image"/>
+        <div style="padding: 14px">
+          <div class="card-con">
+            <span>Apifox</span>
+            <el-button text class="button">
+              <el-icon>
+                <View/>
+              </el-icon>
+            </el-button>
+          </div>
+        </div>
+      </el-card></li>
+  </ul>
+</template>
+
+<script>
+import {View} from '@element-plus/icons-vue'
+export default {
+  name: "AppList",
+  components:{
+    View
+  }
+}
+</script>
+
+<style scoped>
+.app-list{
+  display: grid;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  grid-template-columns: repeat(auto-fill, 300px);
+  grid-gap: 30px 30px;
+  justify-content: center;
+}
+.image {
+  width: 100%;
+  display: block;
+}
+.card-con{
+  display: flex;
+  line-height: 32px;
+  justify-content: space-between;
+}
+</style>
---
+++ src/components/AppMenu.vue
@@ -0,0 +1,43 @@
+<template>
+  <el-menu
+      default-active="2"
+      class="el-menu-vertical"
+      background-color="#262B33"
+      active-text-color="#ffd04b"
+      text-color="#fff"
+  >
+    <el-menu-item index="1">
+      <el-icon><Location /></el-icon>
+      <span>Navigator Two</span>
+    </el-menu-item>
+    <el-menu-item index="2">
+      <el-icon></el-icon>
+      <span>Navigator Two</span>
+    </el-menu-item>
+    <el-menu-item index="3">
+      <el-icon></el-icon>
+      <span>Navigator Three</span>
+    </el-menu-item>
+    <el-menu-item index="4">
+      <el-icon></el-icon>
+      <span>Navigator Four</span>
+    </el-menu-item>
+  </el-menu>
+</template>
+
+<script>
+import {Location} from '@element-plus/icons-vue'
+export default {
+  name: "AppMenu",
+  components:{
+    Location
+  }
+}
+</script>
+
+<style scoped>
+.el-menu-vertical{
+  height: 100%;
+  border-right: 1px solid #2c3e50;
+}
+</style>
